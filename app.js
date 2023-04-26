f7f097d1c33f9dfdecd7ce1c4c6e289958cd5d3f
Mohamed  El karmi <mel-karm@e1r5p3.1337.ma>
2023-04-26 13:50:13 +0100

    push

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ let add = document.querySelector(".add");
 let list = document.querySelector(".list");
 let result = document.querySelector(".all");
 let number = document.querySelector("#nm");
+let clear = document.querySelector(".clear");
+let but_t = document.querySelector(".mod");
 var globalRm;
+let state = false;
 let i = 0;
 let completed = 0;
 
@@ -19,8 +22,19 @@ function addTodo(){
     cross.setAttribute("src", "./images/icon-cross.svg")
     cross.className = "cross";
     newDiv.appendChild(p);
-    newDiv.className = "todos";
+    newDiv.className = `todos`;
     newDiv.appendChild(cross);
+    if (state == false)
+    {
+        newDiv.style.backgroundColor = "hsl(235, 19%, 35%)";
+        newDiv.style.color = "hsl(0, 0%, 98%)";
+
+    }
+    else
+    {
+        newDiv.style.backgroundColor = "hsl(0, 0%, 98%)";
+        newDiv.style.color = "hsl(235, 19%, 35%)";
+    }
     p.className = `check${i}`;
     globalRm = document.querySelector(".cross");
     list.insertBefore(newDiv, list.children[0]);
@@ -28,29 +42,74 @@ function addTodo(){
     button.onclick = function (){
         if (par.style.textDecoration == "line-through")
         {
-          par.style.textDecoration = "none";
-          button.style.backgroundImage = "none";
-          completed--;
+            par.style.textDecoration = "none";
+            button.style.backgroundImage = "none";
+            completed--;
             result.style.display = "none";
         }
-    else
-    {
-        par.style.textDecoration = "line-through";
-        button.style.backgroundImage = "linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
-        completed++;
-        if (completed == i)
+        else
         {
-            result.style.display = "flex";
+            par.style.textDecoration = "line-through";
+            button.style.backgroundImage = "linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
+            completed++;
+            if (completed == i)
+                result.style.display = "flex";
         }
-    }
-};
-cross.onclick = function (){
-    newDiv.remove();
-};
+    };
     i++;
+    cross.onclick = function (){
+        newDiv.remove();
+        i--;
+        number.innerHTML = i;
+    };
     number.innerHTML = i;
 }
 
+
+clear.addEventListener("click", function (){
+    var x = document.querySelectorAll(".todos"); 
+    x.forEach(e =>{
+        e.remove();
+    })
+    number.innerHTML = 0;
+})
+
+
+
+but_t.addEventListener("click", function changeMode(){
+    if (state == false)
+    {
+        var x = document.querySelectorAll(".todos"); 
+        x.forEach(e =>{
+            e.style.color = "black";
+            e.style.backgroundColor = "hsl(0, 0%, 98%)";
+        })
+        input_t.style.backgroundColor = "hsl(0, 0%, 98%)";
+        inputItself.style.color = "black";
+        body.style.backgroundColor = "hsl(236, 33%, 92%)";
+        info.style.backgroundColor = "hsl(0, 0%, 98%)";
+        list_t.style.backgroundColor = "hsl(0, 0%, 98%)";
+        info.style.color = "black";
+        img.setAttribute("src", "./images/icon-moon.svg")
+        state = true;
+    }
+    else
+    {
+        var x = document.querySelectorAll(".todos"); 
+        x.forEach(e =>{
+            e.style.color = "white";
+            e.style.backgroundColor = "hsl(235, 19%, 35%)";
+        })
+        input_t.style.backgroundColor = "hsl(235, 19%, 35%)";
+        inputItself.style.color = "white";
+        body.style.backgroundColor = "hsl(235, 21%, 11%)";
+        info.style.backgroundColor = "hsl(235, 19%, 35%)";
+        list_t.style.backgroundColor = "hsl(0, 0%, 98%)";
+        info.style.color = "white";
+        img.setAttribute("src", "./images/icon-moon.svg")
+        state = false;
+    }
+});
 // if (i != 0)
 // {
     
--- light_mode.js
+++ light_mode.js
@@ -1,19 +1,36 @@
 let body = document.querySelector("body");
 let list_t = document.querySelector(".list");
 let img = document.querySelector("img");
-let state = false;
+let info = document.querySelector(".info");
+let todos = document.querySelectorAll(".todos");
+let input_t = document.querySelector(".input");
+let inputItself = document.querySelector("input");
 
-function changeMode(){
-    if (state == false)
-    {
-        body.style.backgroundColor = "white";
-        img.setAttribute("src", "./images/icon-moon.svg")
-        state = true;
-    }
-    else
-    {
-        body.style.backgroundColor = "hsl(235, 21%, 11%)";
-        img.setAttribute("src", "./images/icon-sun.svg")
-        state = false;
-    }
-}
+// function changeMode(){
+//     if (state == false)
+//     {
+//         input_t.style.backgroundColor = "hsl(0, 0%, 98%)";
+//         inputItself.style.color = "black";
+//         body.style.backgroundColor = "hsl(236, 33%, 92%)";
+//         info.style.backgroundColor = "hsl(0, 0%, 98%)";
+//         list_t.style.backgroundColor = "hsl(0, 0%, 98%)";
+//         info.style.color = "black";
+//         img.setAttribute("src", "./images/icon-moon.svg")
+//         state = true;
+//     }
+//     else
+//     {
+//         body.style.backgroundColor = "hsl(235, 21%, 11%)";
+//         img.setAttribute("src", "./images/icon-sun.svg")
+//         state = false;
+//     }
+// }
+
+// but_t.addEventListener("click", function (){
+//     x.forEach(e =>{
+//         console.log(e);
+//         e.style.color = "black";
+//         e.style.backgroundColor = "hsl(0, 0%, 98%)";
+//     })
+    
+// });
